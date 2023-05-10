fe1f10a2ef625658b35848d61a6464864080d9ca
ClareBear11 <scottclarence5@gmail.com>
2023-05-10 10:11:30 -0500

    Updated index, css and javascript.
    I've added an navigation to the 'left side' on the file. This navigation has been equipped with a simple javascript function. Later I will add the styling for the navigation menu and then hopefully proceed to the mobile friendly view as I prep the main section of the site.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+function showNav(){
+    if (document.getElementById("nav").style.display === "none"){
+        document.getElementById("nav").style.display = "block";
+    }
+    else{
+        document.getElementById("nav").style.display = "none";
+    }
+}
